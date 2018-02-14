76f45cc0b2e6e516110a1c4b202a8f538de2ae87
Levente Vig <levivig@gmail.com>
2018-02-14 19:17:39 +0100

    finished 1st and 2nd lesson exercises

--- 1_hdbms.sql
+++ 1_hdbms.sql
@@ -1,14 +1,14 @@
 -- 101.
 SELECT
   CUST_FIRST_NAME || ' ' || CUST_LAST_NAME AS Name,
-  to_char(DATE_OF_BIRTH, 'DS')             AS "Date of birth"
+  to_char(DATE_OF_BIRTH, 'YYYY.MM.DD.')             AS "Date of birth"
 FROM OE.CUSTOMERS
 ORDER BY DATE_OF_BIRTH DESC, Name;
 
 -- PL/SQL block
 BEGIN
   HDBMS18.MEGOLDAS_FELTOLT(101, '
-      SELECT CUST_FIRST_NAME || '' '' || CUST_LAST_NAME AS Name, to_char(DATE_OF_BIRTH, ''DS'') as "Date of birth"
+      SELECT CUST_FIRST_NAME || '' '' || CUST_LAST_NAME AS Name, to_char(DATE_OF_BIRTH, ''YYYY.MM.DD.'') as "Date of birth"
       FROM OE.CUSTOMERS
       ORDER BY DATE_OF_BIRTH desc, Name;/');
 END;
@@ -18,7 +18,7 @@ END;
 SELECT
   c.CUSTOMER_ID                                AS "ID",
   c.CUST_FIRST_NAME || ' ' || c.CUST_LAST_NAME AS Name,
-  to_char(max(o.order_date), 'DS')             AS "Date"
+  to_char(max(o.order_date), 'YYYY.MM.DD.')             AS "Date"
 FROM OE.CUSTOMERS c
   LEFT JOIN OE.ORDERS o
     ON c.CUSTOMER_ID = o.CUSTOMER_ID
@@ -27,7 +27,7 @@ ORDER BY "Date";
 
 BEGIN
   HDBMS18.MEGOLDAS_FELTOLT(102, '
-SELECT c.CUSTOMER_ID, c.CUST_FIRST_NAME || '' '' || c.CUST_LAST_NAME as Name, to_char(max(o.order_date), ''DS'')
+SELECT c.CUSTOMER_ID, c.CUST_FIRST_NAME || '' '' || c.CUST_LAST_NAME as Name, to_char(max(o.order_date), ''YYYY.MM.DD.'')
 FROM OE.CUSTOMERS c
 LEFT JOIN OE.ORDERS o
   ON c.CUSTOMER_ID=o.CUSTOMER_ID
@@ -68,23 +68,19 @@ CREATE TABLE ORDERS
 
 UPDATE ORDERS o
 SET o.ORDER_TOTAL = o.ORDER_TOTAL * 0.9
-WHERE o.ORDER_ID IN (
-  SELECT o.ORDER_ID
+WHERE o.CUSTOMER_ID IN (
+  SELECT c.CUSTOMER_ID
   FROM oe.CUSTOMERS c
-    JOIN ORDERS o
-      ON c.customer_id = o.customer_id
   WHERE months_between(o.order_date, c.date_of_birth) / 12 < 20
 );
 
 BEGIN
   HDBMS18.MEGOLDAS_FELTOLT(104, 'UPDATE ORDERS o
-    SET o.ORDER_TOTAL = o.ORDER_TOTAL * 0.9
+SET o.ORDER_TOTAL = o.ORDER_TOTAL * 0.9
 WHERE o.ORDER_ID IN (
-    SELECT o.ORDER_ID
-    FROM oe.CUSTOMERS c
-    JOIN ORDERS o
-      ON c.customer_id=o.customer_id
-    WHERE months_between(o.order_date, c.date_of_birth)/12 < 20
+  SELECT c.CUSTOMER_ID
+  FROM oe.CUSTOMERS c
+  WHERE months_between(o.order_date, c.date_of_birth) / 12 < 20
 );/');
 END;
 
@@ -92,7 +88,7 @@ END;
 -- 105.
 SELECT
   o.ORDER_ID,
-  to_char(o.ORDER_DATE, 'DS HH24:MM:SS')       AS "Order Date",
+  to_char(o.ORDER_DATE, 'YYYY.MM.DD. HH24:MM:SS')       AS "Order Date",
   c.CUST_FIRST_NAME || ' ' || c.CUST_LAST_NAME AS "Customer Name"
 FROM oe.ORDERS o
   JOIN oe.ORDER_ITEMS i
@@ -103,7 +99,7 @@ GROUP BY o.ORDER_ID, o.ORDER_DATE, c.CUST_FIRST_NAME || ' ' || c.CUST_LAST_NAME
 HAVING count(*) > 5;
 
 BEGIN
-  HDBMS18.MEGOLDAS_FELTOLT(105, 'SELECT o.ORDER_ID, to_char(o.ORDER_DATE, ''DS HH24:MM:SS'') as "Order Date", c.CUST_FIRST_NAME || '' '' || c.CUST_LAST_NAME as "Customer Name"
+  HDBMS18.MEGOLDAS_FELTOLT(105, 'SELECT o.ORDER_ID, to_char(o.ORDER_DATE, ''YYYY.MM.DD. HH24:MM:SS'') as "Order Date", c.CUST_FIRST_NAME || '' '' || c.CUST_LAST_NAME as "Customer Name"
                                   FROM oe.ORDERS o
                                 JOIN oe.ORDER_ITEMS i
                                     ON o.ORDER_ID = i.ORDER_ID
@@ -162,7 +158,14 @@ END;
 
 
 -- 108.
-INSERT INTO ORDERS
+INSERT INTO ORDERS (ORDER_ID,
+                    ORDER_DATE,
+                    ORDER_MODE,
+                    CUSTOMER_ID,
+                    ORDER_STATUS,
+                    ORDER_TOTAL,
+                    SALES_REP_ID,
+                    PROMOTION_ID)
 VALUES (3000, TO_DATE('2018/02/09 17:36:25', 'YYYY/MM/DD HH24:MI:SS'), 'direct', 100, 0, 999999, 153, NULL);
 
 SELECT *
@@ -170,8 +173,15 @@ FROM ORDERS
 WHERE CUSTOMER_ID = 100;
 
 BEGIN
-  HDBMS18.MEGOLDAS_FELTOLT(108, 'INSERT INTO ORDERS
-VALUES (3000, TO_DATE(''2018/02/09 17:36:25'', ''YYYY/MM/DD HH24:MI:SS''), ''direct'', 100, 0, 999999, 153, NULL );/');
+  HDBMS18.MEGOLDAS_FELTOLT(108, 'INSERT INTO ORDERS (ORDER_ID,
+                    ORDER_DATE,
+                    ORDER_MODE,
+                    CUSTOMER_ID,
+                    ORDER_STATUS,
+                    ORDER_TOTAL,
+                    SALES_REP_ID,
+                    PROMOTION_ID)
+VALUES (3000, TO_DATE(''2018/02/09 17:36:25'', ''YYYY/MM/DD HH24:MI:SS''), ''direct'', 100, 0, 999999, 153, NULL);/');
 END;
 
 
@@ -224,3 +234,104 @@ END;
 SELECT count(*) AS "Accepted solutions"
 FROM HDBMS18.MEGOLDASAIM
 WHERE JO = 'i';
+
+SELECT
+  FELADAT_AZON,
+  MEGJEGYZES
+FROM HDBMS18.MEGOLDASAIM
+WHERE JO = 'h';
+
+-------------------------------------------------------------------
+
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Hello World!');
+END;
+
+DECLARE
+  v_Nagyobb NUMBER;
+  x         NUMBER := 7;
+  y         NUMBER := 6;
+BEGIN
+  v_Nagyobb := x;
+  IF x < y
+  THEN v_Nagyobb := y;
+  END IF;
+END;
+
+
+DECLARE
+  v_Nagyobb NUMBER;
+  x         NUMBER := 5;
+  y         NUMBER := 6;
+BEGIN
+  IF x < y
+  THEN v_Nagyobb := y;
+  ELSE v_Nagyobb := x;
+  END IF;
+END;
+
+
+DECLARE
+  v_Nagyobb NUMBER;
+  x         NUMBER := 5;
+  y         NUMBER := 7;
+BEGIN
+  IF x < y
+  THEN
+    DBMS_OUTPUT.PUT_LINE('x kisebb, mint y');
+    v_Nagyobb := y;
+  ELSIF x > y
+    THEN
+      DBMS_OUTPUT.PUT_LINE('x nagyobb, mint y');
+      v_Nagyobb := x;
+  ELSE
+    DBMS_OUTPUT.PUT_LINE('x és y egyenlők');
+    v_Nagyobb := x;
+  END IF;
+END;
+
+
+DECLARE
+  v_Osszeg NUMBER(10) := 0;
+BEGIN
+  FOR i IN 1..100 LOOP
+    v_Osszeg := v_Osszeg + i;
+  END LOOP;
+END;
+
+
+DECLARE
+  v_Osszeg NUMBER(10) := 0;
+BEGIN
+  FOR i IN REVERSE 1..100 LOOP
+    v_Osszeg := v_Osszeg + i;
+  END LOOP;
+END;
+
+
+DECLARE
+  v_Osszeg PLS_INTEGER;
+  i        PLS_INTEGER;
+BEGIN
+  i := 1;
+  v_Osszeg := 0;
+  LOOP
+    v_Osszeg := v_Osszeg + i;
+    EXIT WHEN v_Osszeg >= 100; -- elértük a célt
+    i := i + 1;
+  END LOOP;
+END;
+
+
+DECLARE
+  v_Osszeg PLS_INTEGER := 0;
+BEGIN
+  <<kulso>>
+  FOR i IN 1..100 LOOP
+    FOR j IN 1..i LOOP
+      v_Osszeg := v_Osszeg + i;
+      EXIT kulso
+      WHEN v_Osszeg > 100;
+    END LOOP;
+  END LOOP;
+END;
--- 2_hdbms.sql
+++ 2_hdbms.sql
@@ -1,1 +1,118 @@
 -- 201.
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('Szép az élet');
+END;
+
+BEGIN
+  HDBMS18.MEGOLDAS_FELTOLT(201, 'BEGIN
+  DBMS_OUTPUT.PUT_LINE(''Szép az élet'');
+END;/');
+END;
+
+
+
+-- 202.
+BEGIN
+  DBMS_OUTPUT.PUT_LINE('A mai dátum: ' || to_char(sysdate, 'YYYY.MM.DD. HH24:MM:SS') || ', A userneved: ' || user);
+END;
+
+BEGIN
+  HDBMS18.MEGOLDAS_FELTOLT(202, 'BEGIN
+  DBMS_OUTPUT.PUT_LINE(''A mai dátum: '' || to_char(sysdate, ''YYYY.MM.DD. HH24:MM:SS'') || '', A userneved: '' || user);
+END;/');
+END;
+
+
+-- 203.
+DECLARE
+  x NUMBER := 1;
+BEGIN
+  LOOP
+    DBMS_OUTPUT.PUT_LINE(x ** 2);
+    x := x + 1;
+    EXIT WHEN X > 10;
+  END LOOP;
+END;
+
+BEGIN
+  HDBMS18.MEGOLDAS_FELTOLT(203, 'DECLARE
+  x NUMBER := 1;
+BEGIN
+  LOOP
+    DBMS_OUTPUT.PUT_LINE(x ** 2);
+    x := x + 1;
+    EXIT WHEN X > 10;
+  END LOOP;
+END;/');
+END;
+
+
+-- 204.
+DECLARE
+  x NUMBER(5) := 9;
+BEGIN
+  IF MOD(x, 6) = 0
+  THEN
+    DBMS_OUTPUT.PUT_LINE(x || ' osztható 6-al');
+  ELSIF MOD(x, 2) = 0
+    THEN
+      DBMS_OUTPUT.PUT_LINE(x || ' osztható 2-vel');
+  ELSIF MOD(x, 3) = 0
+    THEN
+      DBMS_OUTPUT.PUT_LINE(x || ' osztható 3-al');
+  END IF;
+END;
+
+
+BEGIN
+  HDBMS18.MEGOLDAS_FELTOLT(204, 'DECLARE
+  x NUMBER(5) := 9;
+BEGIN
+  IF MOD(x, 6) = 0
+  THEN
+    DBMS_OUTPUT.PUT_LINE(x || '' osztható 6-al'');
+  ELSIF MOD(x, 2) = 0
+    THEN
+      DBMS_OUTPUT.PUT_LINE(x || '' osztható 2-vel'');
+  ELSIF MOD(x, 3) = 0
+    THEN
+      DBMS_OUTPUT.PUT_LINE(x || '' osztható 3-al'');
+  END IF;
+END;/');
+END;
+
+
+-- 205.
+DECLARE
+  x    NUMBER := 39;
+  y    NUMBER := 91;
+  x_og NUMBER := x;
+  y_og NUMBER := y;
+  temp NUMBER(10);
+BEGIN
+  WHILE y != 0 LOOP
+    temp := x;
+    x := y;
+    y := mod(temp, y);
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE(x || ' a legnagyobb közös osztó');
+  DBMS_OUTPUT.PUT_LINE((x_og * y_og) / x || ' a legkisebb közös többszörös');
+END;
+
+BEGIN
+  HDBMS18.MEGOLDAS_FELTOLT(205, 'DECLARE
+  x    NUMBER := 39;
+  y    NUMBER := 91;
+  x_og NUMBER := x;
+  y_og NUMBER := y;
+  temp NUMBER(10);
+BEGIN
+  WHILE y != 0 LOOP
+    temp := x;
+    x := y;
+    y := mod(temp, y);
+  END LOOP;
+  DBMS_OUTPUT.PUT_LINE(x || '' a legnagyobb közös osztó'');
+  DBMS_OUTPUT.PUT_LINE((x_og * y_og) / x || '' a legkisebb közös többszörös'');
+END;/');
+END;
